914d20b44cf0a27dc8411e695bdb76a5878898c8
Rádi Olivér <radi.oliver@diak.szbi-pg.hu>
2025-10-06 12:40:29 +0200

    asdf

---
+++ main.js
@@ -0,0 +1,4 @@
+import {Szamitogep} from "./szamitogep.js";
+let szamitogep = new Szamitogep(1024, false);
+
+console.log(szamitogep.kapcsol());
---
+++ szamitogep.js
@@ -0,0 +1,32 @@
+export class Szamitogep {
+
+    constructor(memoria = 1024, allapot = false)   {
+        this._memoria = memoria;
+        this._allapot = allapot;
+    }
+
+    kapcsol(){
+        this._allapot = !this._allapot;
+    }
+
+    programMasol(szam){
+        if (szam < this._memoria){
+            this._memoria = szam;
+            
+        }
+    }
+
+    get memoria(){
+        return this._memoria;
+    }
+    set memoria(_memoria){
+        this._memoria = _memoria;
+    }
+    get allapot(){
+        return this._allapot;
+    }
+    set allapot(_allapot){
+        this._allapot = _allapot;
+    }
+
+}
