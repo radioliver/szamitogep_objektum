7597c7b69007e8facbb95a94215236a1b313e491
Rádi Olivér <radi.oliver@diak.szbi-pg.hu>
2025-10-13 11:01:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ import {Szamitogep} from "./szamitogep.js";
 let alapsSzamitogep = new Szamitogep(1024, false);
 let szamitogep2 = new Szamitogep(2048, false);
 
-console.log(alapsSzamitogep.kapcsol());
-console.log(alapsSzamitogep.programMasol(800));
-console.log(alapsSzamitogep.programMasol(400));
-console.log(alapsSzamitogep.memoria);
-console.log(alapsSzamitogep.allapot);
+console.log(`Számítógép be: ${alapsSzamitogep.kapcsol()}`);
+console.log(`800 mb: ${alapsSzamitogep.programMasol(800)}`);
+console.log(`Számítógép memória: ${alapsSzamitogep.memoria} mb`);
+console.log(`500 mb: ${alapsSzamitogep.programMasol(500)}`);
+console.log(`Számítógép allapot: ${alapsSzamitogep.allapot}`);
 
-console.log(szamitogep2.kapcsol());
-console.log(szamitogep2.programMasol(1));
-console.log(szamitogep2.memoria);
-console.log(szamitogep2.allapot);
+console.log(`\nSzámítógép be: ${szamitogep2.kapcsol()}`);
+console.log(`1 mb: ${szamitogep2.programMasol(1)}`);
+console.log(`Számítógép memória: ${szamitogep2.memoria} mb`);
+console.log(`Számítógép allapot: ${szamitogep2.allapot}`);
